e4e1dc5349283c31139219ab37f68ea78695350f
Aymeric Faucher <aymeric.faucher@gmail.com>
2017-11-27 17:59:05 +0100

    2d test completed

---
+++ Assets/CalibrateButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalibrateButton : MonoBehaviour {
+	public VirtualObject realObject;
+
+	void Start()
+	{
+		this.GetComponent<Button> ().onClick.AddListener (
+			() => realObject.Calibrate (MouseManager.instance.mousePositionTags)
+		);
+	}
+}
