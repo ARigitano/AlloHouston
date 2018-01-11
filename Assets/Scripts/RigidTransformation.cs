e7f567292803a85d3ecccf6c372efe2eabd26f7e
Aymeric Faucher <aymeric.faucher@gmail.com>
2018-01-11 17:50:55 +0100

    Cleaning up and doc

---
+++ Assets/Scripts/RigidTransformation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RigidTransformation {
+	public Quaternion rotation { get; private set; }
+	public Vector3 dist { get; private set; }
+
+	public RigidTransformation (Quaternion rotation, Vector3 position)
+	{
+		this.rotation = rotation;
+		this.dist = position;
+	}
+
+	public static RigidTransformation Average(RigidTransformation[] rigidTransformations)
+	{
+		if (rigidTransformations.Length == 1)
+			return rigidTransformations [0];
+
+		var rotations = new List<Quaternion> ();
+		var dists = new List<Vector3> ();
+
+		foreach (var rigidTransformation in rigidTransformations) {
+			rotations.Add (rigidTransformation.rotation);
+			dists.Add (rigidTransformation.dist);
+		}
+
+		return new RigidTransformation (MathHelper.AverageQuaternion (rotations), MathHelper.AverageVector3 (dists));
+	}
+}
+
--- Assets/Scripts/VirtualObject.cs
+++ Assets/Scripts/VirtualObject.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// A virtual object. This component can align itself to another objet by comparing their respective tags.
+/// </summary>
 public class VirtualObject : MonoBehaviour
 {
 	public PositionTag[] virtualPositionTags;
@@ -12,6 +15,11 @@ public class VirtualObject : MonoBehaviour
 		virtualPositionTags = GetComponentsInChildren<PositionTag> ().OrderBy (x => x.positionTagIndex).ToArray ();
 	}
 
+	/// <summary>
+	/// Gets the center of an array of points.
+	/// </summary>
+	/// <returns>The center of points as a Vector3</returns>
+	/// <param name="points">An array of a points.</param>
 	Vector3 GetCenterOfPoints (Vector3[] points)
 	{
 		var res = Vector3.zero;
@@ -23,36 +31,94 @@ public class VirtualObject : MonoBehaviour
 		return res / (float)points.Length;
 	}
 
-	Quaternion CalcRotationPivot (CalibrationPlane r, CalibrationPlane v, Transform[] vtrans, CalibrationPlane.Pivot pivot)
+	/// <summary>
+	/// Calculates the difference of rotation and position between two planes around a specific pivot point
+	/// </summary>
+	/// <returns>The rotation pivot, the difference of rotation and position between two planes.</returns>
+	/// <param name="r">The real plane.</param>
+	/// <param name="v">The virtual plane, it should a plane on this game object</param>
+	/// <param name="vtrans">The position of the 3 points that define the virtual plane</param>
+	/// <param name="pivot">The pivot point of the rotation.</param>
+	RigidTransformation CalcRotationPivot (CalibrationPlane r, CalibrationPlane v, Transform[] vtrans, CalibrationPlane.Pivot pivot)
 	{
+		// We save the values of the current rotation and position to restore them at the end of the method
+		Quaternion startRotation = this.transform.rotation;
+		Vector3 startPosition = this.transform.position;
+
+		// We calculate n, the normal vector between the two normal of the calibration planes
 		Vector3 n = Vector3.Cross (r.normal, v.normal);
 
+		// We make sure that all the points in the virtual calibration plane are corresponding to their transform value
+		v.SetPoints (vtrans);
+
+		// We compute the 3 euler angles.
+		// Alpha is the rotation to align the virtual plane to the intersection of the two planes.
+		// Beta is the rotation to make the two planes parallel.
+		// Gamma is the rotation to align the points of the two planes now that the planes are parralel to each other.
 		float alpha = (n != Vector3.zero && v.PivotVect (pivot) != Vector3.zero) ? Vector3.SignedAngle (v.PivotVect (pivot), n, v.normal) : 0.0f;
 		float beta = (v.normal != Vector3.zero && r.normal != Vector3.zero) ? Vector3.SignedAngle (v.normal, r.normal, n) : 0.0f;
 		float gamma = (n != Vector3.zero && r.PivotVect (pivot) != Vector3.zero) ? Vector3.SignedAngle (n, r.PivotVect (pivot), r.normal) : 0.0f;
 	
+		// Alpha rotation around the normal axis
 		this.transform.RotateAround (v.PivotPoint (pivot), v.normal, alpha);
 
+		// Once the rotation is done, we update the points
 		v.SetPoints (vtrans);
 
+		// Beta rotation around the pivot vect
 		this.transform.RotateAround (v.PivotPoint (pivot), v.PivotVect (pivot), beta);
 
+		// Once the rotation is done
 		v.SetPoints (vtrans);
 	
+		// Gamma rotation
 		this.transform.RotateAround (v.PivotPoint (pivot), v.normal, gamma);
 
-		return Quaternion.identity;
+
+		// The two planes are now parallel and aligned, we can compute the difference in rotation between the start and now.
+		var rigidTransformation = new RigidTransformation (Quaternion.Inverse(startRotation) * this.transform.rotation, this.transform.position - startPosition);
+
+		// We set the position and rotation back to where it was.
+		this.transform.rotation = startRotation;
+		this.transform.position = startPosition;
+
+		// We then reset the point in the virtual plane
+		v.SetPoints (vtrans);
+
+		return rigidTransformation;
 	}
 
-	Quaternion CalcRotation (CalibrationPlane r, CalibrationPlane v, Transform[] vtrans)
+	/// <summary>
+	/// Calculates the difference of rotation and position by making an average of the rigid transformations for each of the 3 pivot points.
+	/// </summary>
+	/// <returns>The rotation pivot, the difference of rotation and position between two planes.</returns>
+	/// <param name="r">The real plane.</param>
+	/// <param name="v">The virtual plane, it should a plane on this game object</param>
+	/// <param name="vtrans">The position of the 3 points that define the virtual plane</param>
+	RigidTransformation CalcRotation (CalibrationPlane r, CalibrationPlane v, Transform[] vtrans)
 	{
-		CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.I);
-		//CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.J);
-		//CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.K);
+		var rigidTransformations = new RigidTransformation [3];
+		rigidTransformations[0] = CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.I);
+		rigidTransformations[1] = CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.J);
+		rigidTransformations[2] = CalcRotationPivot (r, v, vtrans, CalibrationPlane.Pivot.K);
 
-		return Quaternion.identity;
+		var average = RigidTransformation.Average (rigidTransformations);
+
+		this.transform.rotation *= average.rotation;
+		this.transform.position += average.dist;
+
+		Debug.Log ("average rotation=" + average.rotation.eulerAngles);
+		Debug.Log ("average dist=" + average.dist);
+
+		return average;
 	}
 
+	/// <summary>
+	/// Calculates the difference in scale between the normals of the real plane and the virtual plane.
+	/// </summary>
+	/// <returns>The difference of scale</returns>
+	/// <param name="nr">Normal vector of the real plane</param>
+	/// <param name="nv">Normal vector of the virtual plane</param></param>
 	float CalcScale (Vector3 nr, Vector3 nv)
 	{
 		float scale = Mathf.Sqrt (nr.magnitude / nv.magnitude);
@@ -62,6 +128,12 @@ public class VirtualObject : MonoBehaviour
 		return scale;
 	}
 
+	/// <summary>
+	/// Calculates the difference in position between the real plane and the virtual plane.
+	/// </summary>
+	/// <returns>The difference in posi</returns>
+	/// <param name="r">The red component.</param>
+	/// <param name="v">V.</param>
 	Vector3 CalcDist (CalibrationPlane r, CalibrationPlane v)
 	{
 		Vector3 rcenter = GetCenterOfPoints (new Vector3[]{ r.i, r.j, r.k });
@@ -70,26 +142,32 @@ public class VirtualObject : MonoBehaviour
 
 		var dist = rcenter - vcenter;
 
+		this.transform.position += dist;
+
 		return dist;
 	}
 
+	/// <summary>
+	/// Calibrate the objet to change its rotation, position and scale to match its position tags to the given positions tags.
+	/// </summary>
+	/// <param name="realPositionTags">Real position tags.</param>
 	public void Calibrate (PositionTag[] realPositionTags)
 	{
 		int length = Mathf.Min (realPositionTags.Length, virtualPositionTags.Length);
 		int count = 0;
-		float totalScale = 0;
-		var avgRotation = Quaternion.identity;
-		var cumulativeRotation = Vector4.zero;
-		var totalDist = Vector3.zero;
 		var startScale = this.transform.localScale;
 		var startRotation = this.transform.rotation;
 		var startPosition = this.transform.position;
-		Quaternion? firstRotation = null;
 
+		var scaleList = new List<float> ();
+		var rotationList = new List<Quaternion> ();
+		var distList = new List<Vector3> ();
+
+		// To have the most accurate result we create two planes from all the possible unique combinations of 3 points and calculate an
+		// average of all transformations in rotation, position and scale.
 		for (int i = 0; i < length; i++) {
 			for (int j = i + 1; j < length; j++) {
 				for (int k = j + 1; k < length; k++) {
-
 					var rtrans = new Transform[] {
 						realPositionTags [i].transform,
 						realPositionTags [j].transform,
@@ -106,87 +184,29 @@ public class VirtualObject : MonoBehaviour
 
 					var v = new CalibrationPlane (vtrans);
 
-					/*
-					Vector3 n = Vector3.Cross (r.normal, v.normal);
-
-					Debug.Log ("nr = " + r.normal.normalized);
-					Debug.Log ("nv = " + v.normal.normalized);
-
-					Debug.Log ("N = " + n.normalized);
-
-					float alpha = (n != Vector3.zero && v.ij != Vector3.zero) ? Vector3.SignedAngle  (v.ij, n, v.normal) : 0.0f;
-					float beta = (v.normal != Vector3.zero && r.normal != Vector3.zero) ? Vector3.SignedAngle (v.normal, r.normal, v.ij) : 0.0f;
-					float gamma = (v.ij != Vector3.zero && r.ij != Vector3.zero) ? Vector3.SignedAngle (n, r.ij, v.normal) : 0.0f;
-
-					Debug.Log ("a0 = " + alpha + " b0 = " + beta + " g0 = " + gamma);
-
-					Debug.Log ("IJV initial = " + v.ij.normalized);
-
-					Debug.Log ("Beta 1 =" + beta);
-
-					// FIRST TRANSFORMATION
-					this.transform.RotateAround (v.i, v.normal, alpha);
-
-					Debug.LogError ("STOP");
-
-					v.SetPoints (vtrans);
-
-					alpha = (n != Vector3.zero && v.ij != Vector3.zero) ? Vector3.SignedAngle  (v.ij, n, v.normal) : 0.0f;
-					beta = (v.normal != Vector3.zero && r.normal != Vector3.zero) ? Vector3.SignedAngle (v.normal, r.normal, v.ij) : 0.0f;
-					gamma = (v.ij != Vector3.zero && r.ij != Vector3.zero) ? Vector3.SignedAngle (v.ij, r.ij, v.normal) : 0.0f;
-
-					Debug.Log ("a1 = " + alpha + " b1 = " + beta + " g1 = " + gamma);
-					
-					Debug.Log ("IJV after = " + v.ij.normalized);
-
-					// SECOND TRANSFORMATION
-					this.transform.RotateAround (v.i, v.ij, beta);
-
-					Debug.LogError ("STOP");
-
-					v.SetPoints (vtrans);
-
-					alpha = (n != Vector3.zero && v.ij != Vector3.zero) ? Vector3.SignedAngle  (v.ij, n, v.normal) : 0.0f;
-					beta = (v.normal != Vector3.zero && r.normal != Vector3.zero) ? Vector3.SignedAngle (v.normal, r.normal, v.ij) : 0.0f;
-					gamma = (v.ij != Vector3.zero && r.ij != Vector3.zero) ? Vector3.SignedAngle (v.ij, r.ij, v.normal) : 0.0f;
-
-					Debug.Log ("a2 = " + alpha + " b2 = " + beta + " g2 = " + gamma);
-
-					beta = (v.normal != Vector3.zero && r.normal != Vector3.zero) ? Vector3.Angle (v.normal, r.normal) : 0.0f;
-
-					Debug.Log ("Beta 3 = " + beta);
-
-					// THIRD TRANSFORMATION
-					this.transform.RotateAround (v.i, v.normal, gamma);
-
-					Debug.LogError ("STOP");
-					*/
-
-					var rotation = CalcRotation (r, v, vtrans);
+					var rigidTransformation = CalcRotation (r, v, vtrans);
 
 					var scale = CalcScale (r.normal, v.normal);
-
 
 					v.SetPoints (vtrans);
 
 					var dist = CalcDist (r, v);
 					count++;
 
-					//avgRotation = rotation;
-					//avgRotation = MathHelper.AverageQuaternion(ref cumulativeRotation, rotation, firstRotation.Value, count);
-					totalScale += scale;
-					totalDist += dist;
+					// We add the calculated transformation to the corresponding lists
+					rotationList.Add(rigidTransformation.rotation);
+					scaleList.Add (scale);
+					distList.Add (rigidTransformation.dist + dist);
 
-					Debug.Log (avgRotation);
-
-					//this.transform.position = startPosition;
-					//this.transform.localScale = startScale;
-					// this.transform.rotation = startRotation;
+					// Once the transformations are saved, we can go back to the starting position
+					this.transform.localScale = startScale;
+					this.transform.rotation = startRotation;
+					this.transform.position = startPosition;
 				}
 			}
 		}
-		//this.transform.localScale *= totalScale / (float)count;
-		//this.transform.rotation *= avgRotation;
-		this.transform.position += totalDist / (float)count;
+		this.transform.rotation *= MathHelper.AverageQuaternion (rotationList);
+		this.transform.localScale *= scaleList.Average();
+		this.transform.position += MathHelper.AverageVector3 (distList);
 	}
 }
