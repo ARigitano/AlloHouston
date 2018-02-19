aa6abb5bbdd1880576efd9a644c17661bcc9f0a7
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-02-19 14:01:26 +0100

    Calibration with Vive Controller

--- Assets/Scripts/ViveControllerManager.cs
+++ Assets/Scripts/ViveControllerManager.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 namespace VRCalibrationTool
 {
-	public class ViveControllerManager : MonoBehaviour {
+	public class ViveControllerManager : MonoBehaviour
+	{
 
-		public SteamVR_TrackedObject trackedObj;
+		[SerializeField] private SteamVR_TrackedObject _trackedObj;
+		[SerializeField] private PositionTag _positionTag;
+		public PositionTag[] PositionTags;
+		private int indexPositionTag = 0;
 
 		// Use this for initialization
-		void Start () {
+		void Start ()
+		{
 		
+		}
+
+		/// <summary>
+		/// Creates a new position tag at the position of the controller.
+		/// </summary>
+		void CreatePositionTag ()
+		{
+			PositionTag pTag = (PositionTag)Instantiate (_positionTag, _trackedObj.transform.position, Quaternion.identity);
+			pTag.GetComponent<PositionTag> ().positionTagIndex = indexPositionTag;
+			PositionTags [indexPositionTag] = pTag;
+			indexPositionTag++;
+			Debug.Log ("New position tag created");
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-
-			SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index);
+			SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)_trackedObj.index);
 			if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
-				Debug.Log ("Trigger pressed");
-				var controllerPosition = trackedObj.transform.position;
-				MouseManager().CreatePositionTag cPT = new VRCalibrationTool.MouseManager().CreatePositionTag (controllerPosition);
+				if (indexPositionTag < 3) {
+					CreatePositionTag ();
+				} else {
+					GameObject VirtualObject = GameObject.Find ("Cube");
+					if (VirtualObject.GetComponent<VirtualObject> () != null) {
+						VirtualObject.GetComponent<VirtualObject> ().Calibrate (PositionTags);
+					}
+				}
 			}
 		
 		}
+
 	}
 }
