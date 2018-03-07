b173ad1c52936c376b373ec79e0f60b2195807c8
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-03-07 18:52:57 +0100

    Deleting incorrect calibration points

---
+++ Assets/VivePointer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VRCalibrationTool
+{
+public class VivePointer : MonoBehaviour {
+
+	
+		[SerializeField] private ViveControllerManager viveManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "PositionTag") {
+			viveManager.touchingPoint = true;
+				viveManager.incorrectPoint = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "PositionTag") {
+				viveManager.touchingPoint = false;
+				viveManager.incorrectPoint = null;
+		}
+	}
+}
+}
