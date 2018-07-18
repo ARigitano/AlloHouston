c21a9f8b7f6061f96d19b140a855579be2959a4f
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-07-18 11:50:36 +0200

    Second experiment

--- Assets/Scripts/Experiments/Experimentation.cs
+++ Assets/Scripts/Experiments/Experimentation.cs
@@ -7,23 +7,61 @@ using UnityEngine.UI;
 public class Experimentation : MonoBehaviour {
 
     public TextMesh _expNumber;             //Number of this instance of the experiment in the room
-    public RoomManager _roomManager;
-    public Room table;
+    protected RoomManager _roomManager;
+    protected Room table;
     public GameObject textPrefab;           //Prefab of the error text for the table screen
     public GameObject panelToAttachTextTo;  //Errors panel of the table screen
     public int error;                       //Number of the error for this instance of the experiment
-    public GameObject text;                //Error text displayed on the table screen
+    protected GameObject text;                //Error text displayed on the table screen
     public bool _fixed = false;
     public string _errorReference;
+    protected GameManager _gameManager;
+    public AudioSource _success, _fail;
 
+       protected void AttachPanel()
+    {
+        panelToAttachTextTo = table.canvas;
+        text = (GameObject)Instantiate(textPrefab);
+        text.transform.SetParent(panelToAttachTextTo.transform);
+        text.transform.localRotation = Quaternion.identity;
+        text.transform.position = panelToAttachTextTo.transform.position;
+    }
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        table = GameObject.FindGameObjectWithTag("Table").GetComponent<Room>();
+    }
+
+
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// Displays a status message depending of the outcome of the experiment
+    /// </summary>
+    /// <param name="index">Number chose by pressin a colored button</param>
+    public void Resolved(int index)
+    {
+        if (index == error)
+        {
+            Debug.Log("Experiment solved");
+            _success.Play();
+            text.transform.GetComponent<Text>().text = _expNumber.text + ": Cleared";
+            _expNumber.color = Color.green;
+            _gameManager.incidentsFixed++;
+            _gameManager.EndGame();
+        }
+        else
+        {
+            Debug.Log("Experiment failed");
+            _fail.Play();
+            text.transform.GetComponent<Text>().text = _expNumber.text + ": Failed";
+            _expNumber.color = Color.red;
+        }
+    }
 }
--- Assets/Scripts/Prototype/ColorXP.cs
+++ Assets/Scripts/Prototype/ColorXP.cs
@@ -10,45 +10,36 @@ using UnityEngine.UI;
 
 public class ColorXP : Experimentation {
 
-    /*public TextMesh _expNumber;             //Number of this instance of the experiment in the room
-	private RoomManager _roomManager;
-    private Room table;
-    public GameObject textPrefab;           //Prefab of the error text for the table screen
-    public GameObject panelToAttachTextTo;  //Errors panel of the table screen
-    public int error;                       //Number of the error for this instance of the experiment
-    private GameObject text;                //Error text displayed on the table screen*/
-    private GameManager _gameManager;
-    public AudioSource _success, _fail;
-
     // Use this for initialization
     void Start ()
     {
-        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        table = GameObject.FindGameObjectWithTag("Table").GetComponent<Room>();
+        
 
         if (table != null && table.canvas)
         {
-            panelToAttachTextTo = table.canvas;
-            text = (GameObject)Instantiate(textPrefab);
-            text.transform.SetParent(panelToAttachTextTo.transform);
-            text.transform.localRotation = Quaternion.identity;
-            text.transform.position = panelToAttachTextTo.transform.position;
-            error = Random.Range(0, 3);
+            AttachPanel();
+            error = Random.Range(0, 5);
 
             switch(error)
             {
 
                     case 0:
-                    _errorReference = "AB484";
+                    _errorReference = "He";
                     break;
                     case 1:
-                    _errorReference = "KJ208";
+                    _errorReference = "Au";
                     break;
                     case 2:
-                    _errorReference = "CR101";
+                    _errorReference = "Ag";
                     break;
                 case 3:
-                    _errorReference = "AH332";
+                    _errorReference = "K";
+                    break;
+                case 4:
+                    _errorReference = "O";
+                    break;
+                case 5:
+                    _errorReference = "C";
                     break;
                 default:
                     break;
@@ -58,26 +49,5 @@ public class ColorXP : Experimentation {
         }
     }
 
-    /// <summary>
-    /// Displays a status message depending of the outcome of the experiment
-    /// </summary>
-    /// <param name="index">Number chose by pressin a colored button</param>
-    public void Resolved(int index)
-    {
-        if(index == error)
-        {
-            Debug.Log("Experiment solved");
-            _success.Play();
-            text.transform.GetComponent<Text>().text = _expNumber.text + ": Cleared";
-            _expNumber.color = Color.green;
-            _gameManager.incidentsFixed++;
-            _gameManager.EndGame();
-        } else
-        {
-            Debug.Log("Experiment failed");
-            _fail.Play();
-            text.transform.GetComponent<Text>().text = _expNumber.text + ": Failed";
-            _expNumber.color = Color.red;
-        }
-    }
+
 }
