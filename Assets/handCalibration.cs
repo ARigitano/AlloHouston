0a5b1bdfde33683a50c19918c1e6f8bf5b6bb2ce
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-10-16 15:13:31 +0200

    Manus left hand calibration

--- Assets/handCalibration.cs
+++ Assets/handCalibration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.ManusVR.Scripts;
 
 public class handCalibration : MonoBehaviour {
 
@@ -8,36 +9,45 @@ public class handCalibration : MonoBehaviour {
     public float moveNumber;
     public int rotateNumber;
 
+    public TrackingManager manus;
+
 	
 
     public void XMoveLeft()
     {
-        cube.transform.position += new Vector3(-moveNumber, 0f, 0f);
+        manus.LCustomPositionOffset += new Vector3(-moveNumber, 0f, 0f);
+        //cube.transform.position += new Vector3(-moveNumber, 0f, 0f);
     }
 
     public void XMoveRight()
     {
-        cube.transform.position += new Vector3(moveNumber, 0f, 0f);
+        manus.LCustomPositionOffset += new Vector3(moveNumber, 0f, 0f);
+        //cube.transform.position += new Vector3(moveNumber, 0f, 0f);
     }
 
     public void YMoveLeft()
     {
-        cube.transform.position += new Vector3(0f, moveNumber, 0f);
+       
+        manus.LCustomPositionOffset += new Vector3(0f, moveNumber, 0f);
+        //cube.transform.position += new Vector3(0f, moveNumber, 0f);
     }
 
     public void YMoveRight()
     {
-        cube.transform.position += new Vector3(0f, -moveNumber, 0f);
+        manus.LCustomPositionOffset += new Vector3(0f, -moveNumber, 0f);
+        //cube.transform.position += new Vector3(0f, -moveNumber, 0f);
     }
 
     public void ZMoveLeft()
     {
-        cube.transform.position += new Vector3(0f, 0f, moveNumber);
+        manus.LCustomPositionOffset += new Vector3(0f, 0f, moveNumber);
+        //cube.transform.position += new Vector3(0f, 0f, moveNumber);
     }
 
     public void ZMoveRight()
     {
-        cube.transform.position += new Vector3(0f, 0f, -moveNumber);
+        manus.LCustomPositionOffset += new Vector3(0f, 0f, -moveNumber);
+        //cube.transform.position += new Vector3(0f, 0f, -moveNumber);
     }
 
 
@@ -45,32 +55,33 @@ public class handCalibration : MonoBehaviour {
 
     public void XRotateLeft()
     {
-        cube.transform.eulerAngles += new Vector3(rotateNumber, 0f, 0f);
+        manus.LCustomRotationOffset += new Vector3(rotateNumber, 0f, 0f);
+        //cube.transform.eulerAngles += new Vector3(rotateNumber, 0f, 0f);
     }
 
     public void XRotateRight()
     {
-        cube.transform.eulerAngles += new Vector3(-rotateNumber, 0f, 0f);
+        //cube.transform.eulerAngles += new Vector3(-rotateNumber, 0f, 0f);
     }
 
     public void YRotateLeft()
     {
-        cube.transform.eulerAngles += new Vector3(0f, -rotateNumber, 0f);
+        //cube.transform.eulerAngles += new Vector3(0f, -rotateNumber, 0f);
     }
 
     public void YRotateRight()
     {
-        cube.transform.eulerAngles += new Vector3(0f, rotateNumber, 0f);
+        //cube.transform.eulerAngles += new Vector3(0f, rotateNumber, 0f);
     }
 
     public void ZRotateLeft()
     {
-        cube.transform.eulerAngles += new Vector3(0f, 0f, rotateNumber);
+        //cube.transform.eulerAngles += new Vector3(0f, 0f, rotateNumber);
     }
 
     public void ZRotateRight()
     {
-        cube.transform.eulerAngles += new Vector3(0f, 0f, -rotateNumber);
+        //cube.transform.eulerAngles += new Vector3(0f, 0f, -rotateNumber);
     }
 
     
