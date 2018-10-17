f39865059479b10d1eda7010e74b1d83f33310ea
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-10-17 18:29:21 +0200

    Menu language values

--- Assets/handCalibration.cs
+++ Assets/handCalibration.cs
@@ -89,7 +89,87 @@ public class handCalibration : MonoBehaviour {
         //cube.transform.eulerAngles += new Vector3(0f, 0f, -rotateNumber);
     }
 
-    
+
+
+
+
+    public void XMoveLeft2()
+    {
+        manus.RCustomPositionOffset += new Vector3(-moveNumber, 0f, 0f);
+        //cube.transform.position += new Vector3(-moveNumber, 0f, 0f);
+    }
+
+    public void XMoveRight2()
+    {
+        manus.RCustomPositionOffset += new Vector3(moveNumber, 0f, 0f);
+        //cube.transform.position += new Vector3(moveNumber, 0f, 0f);
+    }
+
+    public void YMoveLeft2()
+    {
+
+        manus.RCustomPositionOffset += new Vector3(0f, moveNumber, 0f);
+        //cube.transform.position += new Vector3(0f, moveNumber, 0f);
+    }
+
+    public void YMoveRight2()
+    {
+        manus.RCustomPositionOffset += new Vector3(0f, -moveNumber, 0f);
+        //cube.transform.position += new Vector3(0f, -moveNumber, 0f);
+    }
+
+    public void ZMoveLeft2()
+    {
+        manus.RCustomPositionOffset += new Vector3(0f, 0f, moveNumber);
+        //cube.transform.position += new Vector3(0f, 0f, moveNumber);
+    }
+
+    public void ZMoveRight2()
+    {
+        manus.RCustomPositionOffset += new Vector3(0f, 0f, -moveNumber);
+        //cube.transform.position += new Vector3(0f, 0f, -moveNumber);
+    }
+
+
+
+
+    public void XRotateLeft2()
+    {
+        manus.RCustomRotationOffset += new Vector3(rotateNumber, 0f, 0f);
+        //cube.transform.eulerAngles += new Vector3(rotateNumber, 0f, 0f);
+    }
+
+    public void XRotateRight2()
+    {
+        manus.RCustomRotationOffset += new Vector3(-rotateNumber, 0f, 0f);
+        //cube.transform.eulerAngles += new Vector3(-rotateNumber, 0f, 0f);
+    }
+
+    public void YRotateLeft2()
+    {
+        manus.RCustomRotationOffset += new Vector3(0f, -rotateNumber, 0f);
+        //cube.transform.eulerAngles += new Vector3(0f, -rotateNumber, 0f);
+    }
+
+    public void YRotateRight2()
+    {
+        manus.RCustomRotationOffset += new Vector3(0f, rotateNumber, 0f);
+        //cube.transform.eulerAngles += new Vector3(0f, rotateNumber, 0f);
+    }
+
+    public void ZRotateLeft2()
+    {
+        manus.RCustomRotationOffset += new Vector3(0f, 0f, rotateNumber);
+        //cube.transform.eulerAngles += new Vector3(0f, 0f, rotateNumber);
+    }
+
+    public void ZRotateRight2()
+    {
+        manus.RCustomRotationOffset += new Vector3(0f, 0f, -rotateNumber);
+        //cube.transform.eulerAngles += new Vector3(0f, 0f, -rotateNumber);
+    }
+
+
 
 
 }
