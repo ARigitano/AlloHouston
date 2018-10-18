482793033f42d9c1721af1ad0f4bc2e3f16ce041
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-10-18 16:12:29 +0200

    Menu communication AI, timer and cameras

--- Assets/GamePanel.cs
+++ Assets/GamePanel.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using VRCalibrationTool;
 
 public class GamePanel : MonoBehaviour
 {
 
     [SerializeField]
-    private Text title, next, cameras, logs, ai, timer, experiments, end, launch, abort, actions;
+    private Text title, next, cameras, logs, ai, timer, experiments, end, launch, abort, actions, time;
     [SerializeField]
     private Translation translation;
     [SerializeField]
     private Language setLanguage;
+    [SerializeField]
+    private GameManager gameManager;
+    [SerializeField]
+    private AIScreen aiScreen;
+    [SerializeField]
+    private RoomManager roomManager;
+    [SerializeField]
+    private TextMeshPro aiText;
+    [SerializeField]
+    private Camera aiCamera;
+    [SerializeField]
+    private InputField aiCommunication;
+
+
 
     void OnEnable()
     {
@@ -27,5 +43,17 @@ public class GamePanel : MonoBehaviour
         launch.text = translation.gameLaunch;
         abort.text = translation.gameAbort;
         actions.text = translation.gameActions;
+
+        aiScreen = roomManager._aiScreen;
+        aiText = aiScreen._aiText;
+        aiCamera = aiScreen._aiCamera;
+
+
+    }
+
+    private void Update()
+    {
+        time.text =  Mathf.Floor(gameManager._timerSeconds / 60).ToString("00") + ":" + Mathf.Floor(gameManager._timerSeconds % 60).ToString("00");
+        aiText.text = aiCommunication.text;
     }
 }
