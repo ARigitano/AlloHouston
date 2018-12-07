1746985b8574a0602c5c101eb8abbe102ad74c5d
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2018-12-07 19:01:17 +0100

    More trigonometry

--- Assets/FakeHologram.cs
+++ Assets/FakeHologram.cs
@@ -44,8 +44,9 @@ namespace CRI.HelloHouston.ParticlePhysics
         /// <summary>
         /// 
         /// </summary>
-        private float _thetaMin = 0f;
-            //Mathf.PI / 6;
+        //TO DO rendre accessible dans editor
+        private float _thetaMin = 0;
+        //Mathf.PI / 6;
         /// <summary>
         /// 
         /// </summary>
@@ -60,7 +61,7 @@ namespace CRI.HelloHouston.ParticlePhysics
         /// 
         /// </summary>
         private float _phiMax = 0f;
-        //Mathf.PI / 3;
+            //Mathf.PI / 3;
         /// <summary>
         /// 
         /// </summary>
@@ -69,19 +70,36 @@ namespace CRI.HelloHouston.ParticlePhysics
         /// 
         /// </summary>
         private float _amplitudeB = 1f;
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField]
+        private GameObject _particleSpline,
+
+            _destination;
+
+        private float Gamma(float x, float y, float z)
+        {
+            float gamma = Mathf.Atan2(y,Mathf.Sqrt(Mathf.Pow(x, 2)+Mathf.Pow(z, 2)));
+
+            return gamma;
+        }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="amplitude"></param>
-        /// <param name="angleMin"></param>
-        /// <param name="angleMax"></param>
+        /// <param name="x1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y1"></param>
+        /// <param name="y2"></param>
         /// <returns></returns>
-        private float BezierX(float amplitude, float angleMin, float angleMax)
+        private float AngleTrigonometry(float x1, float x4, float y1, float y4)
         {
-            float x = amplitude * Mathf.Sin(Random.Range(angleMin, angleMax));
+            float beta = Mathf.Atan2((y4 - y1),(x4 - x1));
+            Debug.Log("beta"+beta);
+            
 
-            return x;
+            return beta;
         }
 
         /// <summary>
@@ -91,11 +109,25 @@ namespace CRI.HelloHouston.ParticlePhysics
         /// <param name="angleMin"></param>
         /// <param name="angleMax"></param>
         /// <returns></returns>
-        private float BezierY(float amplitude, float angleMin, float angleMax)
+        private float BezierCos(float amplitude, float angleMin, float angleMax, Vector3 firstPoint, Vector3 lastPoint)
         {
-            float y = amplitude * Mathf.Cos(Random.Range(angleMin, angleMax));
+            float res = amplitude * Mathf.Cos(Random.Range(angleMin, angleMax)+AngleTrigonometry(firstPoint.x, lastPoint.x, firstPoint.z, lastPoint.z));
 
-            return y;
+            return res;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amplitude"></param>
+        /// <param name="angleMin"></param>
+        /// <param name="angleMax"></param>
+        /// <returns></returns>
+        private float BezierSin(float amplitude, float angleMin, float angleMax, Vector3 firstPoint, Vector3 lastPoint)
+        {
+            float res = amplitude * Mathf.Sin(Random.Range(angleMin, angleMax)+AngleTrigonometry(firstPoint.x, lastPoint.x, firstPoint.z, lastPoint.z));
+
+            return res;
         }
 
         /// <summary>
@@ -125,10 +157,17 @@ namespace CRI.HelloHouston.ParticlePhysics
         /// <returns>The end position of the line.</returns>
         private Vector3 createLine(int i, Particle particle)
         {
-            lines[i] = new GameObject();
-            lines[i].transform.parent = this.gameObject.transform;
+            lines[i] = (GameObject)Instantiate(_particleSpline, Vector3.zero, Quaternion.identity, transform);
+            
 
-            pointsB[i] = new GameObject();
+            lines[i].transform.localPosition = Vector3.zero;
+            lines[i].transform.localRotation = Quaternion.identity;
+
+            //lines[i] = new GameObject();
+            BezierSpline spline = lines[i].GetComponent<BezierSpline>();
+
+            pointsB[i] = (GameObject)Instantiate(_destination, Vector3.zero, Quaternion.identity);
+            //pointsB[i] = new GameObject();
             pointsB[i].transform.parent = this.gameObject.transform;
 
             if (particle.destination == 2)
@@ -163,26 +202,37 @@ namespace CRI.HelloHouston.ParticlePhysics
 
             //pointsB[i].transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.2f, 0.2f), Random.Range(-0.1f, 0.1f));
             GameObject line = lines[i];
-            line.AddComponent<BezierSpline>();
-            BezierSpline spline = lines[i].GetComponent<BezierSpline>();
+            //line.AddComponent<BezierSpline>();
+            
             spline.Reset();
 
-            spline.points[0] = this.gameObject.transform.position;
-
-            spline.points[1].x = BezierX(_amplitudeA, _thetaMin, _thetaMax) - spline.points[0].x;
-            spline.points[1].y = BezierY(_amplitudeA, _thetaMin, _thetaMax) - spline.points[0].y;
-            spline.points[1].z = spline.points[0].z;
-
-            spline.points[3] = pointsB[i].transform.position;
-
-            spline.points[2].x = BezierX(_amplitudeB, _phiMin, _phiMax) - spline.points[3].x;
-            spline.points[2].y = BezierY(_amplitudeB, _phiMin, _phiMax) - spline.points[3].y;
-            spline.points[2].z = pointsB[i].transform.position.z;
-
-            Debug.Log(spline.points[1]);
-            Debug.Log(spline.points[2]);
-
+            spline.points[0] = Vector3.zero;
+            //spline.points[0] = this.gameObject.transform.position;
             
+
+            //spline.points[3] = pointsB[i].transform.position;
+
+            spline.points[3] = new Vector3(1, 1, 1);
+
+
+            spline.points[1].x = Mathf.Cos(Mathf.PI/6)*(Mathf.Cos(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z))*BezierCos(_amplitudeA, _thetaMin, _thetaMax, spline.points[0], spline.points[3])+spline.points[0].x);
+            spline.points[1].y = Mathf.Sin(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z));
+            spline.points[1].z = Mathf.Sin(Mathf.PI/6)*(Mathf.Cos(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z))*BezierSin(_amplitudeA, _thetaMin, _thetaMax, spline.points[0], spline.points[3])+spline.points[0].z);
+
+            spline.points[2].x = -Mathf.Cos(Mathf.PI/6)*(-(Mathf.Cos(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z))*BezierCos(_amplitudeA, _thetaMin, _thetaMax, spline.points[0], spline.points[3])+spline.points[0].x));
+            spline.points[2].y = -(Mathf.Sin(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z)));
+            spline.points[2].z = -Mathf.Sin(Mathf.PI/6)*(-(Mathf.Cos(Gamma(spline.points[3].x, spline.points[3].y, spline.points[3].z))*BezierSin(_amplitudeA, _thetaMin, _thetaMax, spline.points[0], spline.points[3])+spline.points[0].z));
+
+            /*spline.points[2].x = -BezierCos(_amplitudeB, _phiMin, _phiMax, spline.points[0], spline.points[3]) + spline.points[3].x;
+            spline.points[2].y = spline.points[3].y;
+            spline.points[2].z = -BezierSin(_amplitudeB, _phiMin, _phiMax, spline.points[0], spline.points[3]) + spline.points[3].z;*/
+
+            Debug.Log("point0" + spline.points[0]);
+            Debug.Log("point1" + spline.points[1]);
+            Debug.Log("point2" + spline.points[2]);
+            Debug.Log("point3" + spline.points[3]);
+
+
 
             //pointsB[i].transform.localPosition = new Vector3(x/2, y, z);
 
@@ -193,7 +243,7 @@ namespace CRI.HelloHouston.ParticlePhysics
             {
                 spline.AddCurve();
             }*/
-
+            Debug.Log(lines[i].transform.rotation);
             if (true)
                 //particle.line)
             {
@@ -205,7 +255,7 @@ namespace CRI.HelloHouston.ParticlePhysics
                 decorator.items[0] = _sphere.transform;
                 decorator.Populate();
             }
-
+            Debug.Log(lines[i].transform.rotation);
             return spline.points[3];
         }
         //TO DO
@@ -254,7 +304,7 @@ namespace CRI.HelloHouston.ParticlePhysics
 
         private void Start()
         {
-            //createLine(0);
+           
         }
     }
 }
