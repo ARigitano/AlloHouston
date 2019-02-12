78c4317c0631d8760be3bce022313a99b03f6c18
Gamelab PC <>
2019-02-12 11:27:14 +0100

    head position fixed

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -7,8 +7,6 @@ public class FollowCamera : MonoBehaviour
     [SerializeField]
     private Camera _camera;
     [SerializeField]
-    private Vector3 _offset;
-    [SerializeField]
     private Transform _target;
 
     // Start is called before the first frame update
@@ -20,7 +18,6 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Vector3.MoveTowards(transform.position, _camera.transform.position + _camera.transform.forward +_offset, Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
         transform.LookAt(_camera.transform.position);
     }
