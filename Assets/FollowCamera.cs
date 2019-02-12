62214c5e3f759e120ac1c8a78ab97ffac49eadbf
Gamelab PC <>
2019-02-12 11:58:32 +0100

    hud working

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -10,6 +10,7 @@ public class FollowCamera : MonoBehaviour
     private Transform _target;
     private float _initialRotation;
     private float _secondRotation;
+    private bool _isMoving;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +23,11 @@ public class FollowCamera : MonoBehaviour
         while (true)
         {
             _initialRotation = _camera.transform.eulerAngles.y;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
             _secondRotation = _camera.transform.eulerAngles.y;
-            if (Mathf.Abs(_initialRotation - _secondRotation) > 10f)
+            if (Mathf.Abs(_initialRotation - _secondRotation) > 30f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
-                transform.LookAt(_camera.transform.position);
-                _secondRotation = _initialRotation;
+                _isMoving = true;
             }
         }
     }
@@ -36,6 +35,15 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(_isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
+            transform.LookAt(_camera.transform.position);
+            if(transform.position == _target.position)
+            {
+                _secondRotation = _initialRotation;
+                _isMoving = false;
+            }
+        }
     }
 }
