c3e022aab57fdfef90ddcab8d5e6dd77d8527fd1
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2019-02-12 11:49:31 +0100

    hud only moves when rotation greater

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -8,17 +8,34 @@ public class FollowCamera : MonoBehaviour
     private Camera _camera;
     [SerializeField]
     private Transform _target;
+    private float _initialRotation;
+    private float _secondRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("DetectRotation");
+    }
+
+    IEnumerator DetectRotation()
+    {
+        while (true)
+        {
+            _initialRotation = _camera.transform.eulerAngles.y;
+            yield return new WaitForSeconds(2f);
+            _secondRotation = _camera.transform.eulerAngles.y;
+            if (Mathf.Abs(_initialRotation - _secondRotation) > 10f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
+                transform.LookAt(_camera.transform.position);
+                _secondRotation = _initialRotation;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
-        transform.LookAt(_camera.transform.position);
+        
     }
 }
