c4e7f1ffc15a6f696c05e48043e043a8734463f1
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2019-02-12 11:12:25 +0100

    position of hud

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -8,6 +8,8 @@ public class FollowCamera : MonoBehaviour
     private Camera _camera;
     [SerializeField]
     private Vector3 _offset;
+    [SerializeField]
+    private Transform _target;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,8 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, _camera.transform.position+_offset, Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, _camera.transform.position + _camera.transform.forward +_offset, Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime);
+        transform.LookAt(_camera.transform.position);
     }
 }
