6636fed0b364cf7126493767ee749954388f27ee
Aymeric Faucher <aymeric.faucher@gmail.com>
2019-02-15 16:54:07 +0100

    MAIAHologramFeynman: shuffled reaction list

--- Assets/Scripts/Extensions/IListExtensions.cs
+++ Assets/Scripts/Extensions/IListExtensions.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CRI.HelloHouston
 {
     public static class IListExtensions
     {
-        public static void Shuffle<T>(this IList<T> ts)
+        public static IList<T> Shuffle<T>(this IList<T> ts)
         {
+            return ts.Shuffle(new Random());
+        }
+
+        public static IList<T> Shuffle<T>(this IList<T> ts, int randomSeed)
+        {
+            return ts.Shuffle(new Random(randomSeed));
+        }
+
+        public static IList<T> Shuffle<T>(this IList<T> ts, Random rand)
+        {
+            IList<T> list = ts.ToList();
             var count = ts.Count;
             var last = count - 1;
             for (var i = 0; i < last; ++i)
             {
-                var r = UnityEngine.Random.Range(i, count);
-                var tmp = ts[i];
-                ts[i] = ts[r];
-                ts[r] = tmp;
+                var r = rand.Next(i, count);
+                var tmp = list[i];
+                list[i] = list[r];
+                list[r] = tmp;
             }
+            return list;
         }
     }
 }
