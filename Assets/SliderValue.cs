e4f7f3b9fc3f4f23bf0179cbab575155b9baa831
Aymeric Faucher <aymeric.faucher@gmail.com>
2019-03-08 18:21:40 +0100

    tablet remake: in progress

---
+++ Assets/SliderValue.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CRI.HelloHouston.UI
+{
+    [ExecuteInEditMode]
+    public class SliderValue : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("A slider.")]
+        private Slider _slider;
+        [SerializeField]
+        [Tooltip("The text on which the value of the slider is displayed.")]
+        private Text _text;
+
+        private void Reset()
+        {
+            _slider = GetComponentInParent<Slider>();
+            _text = GetComponentInChildren<Text>();
+        }
+
+        private void Start()
+        {
+            _slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            _text.text = value.ToString();
+        }
+    }
+}
