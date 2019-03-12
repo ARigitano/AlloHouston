d036612c5c5a29c38d43f04e5c38b432597034c7
Aymeric Faucher <aymeric.faucher@gmail.com>
2019-03-12 19:34:52 +0100

    button trigger test 3

--- Assets/Scripts/Other/ButtonTrigger.cs
+++ Assets/Scripts/Other/ButtonTrigger.cs
@@ -2,33 +2,13 @@
 using UnityEngine.UI;
 using Valve.VR.InteractionSystem;
 
-public class ButtonTrigger : UIElement {
-
-    private bool _controllerIn = false;
-    private bool _activated = false;
-    private Sprite _touchedSprite;
-    private Sprite _untouchedSprite;
-    private Image _buttonImage;
-
-    // Use this for initialization
-    void Start()
-    {
-        _buttonImage = GetComponent<Button>().targetGraphic.GetComponent<Image>();
-        _untouchedSprite = _buttonImage.sprite;
-        _touchedSprite = GetComponent<Button>().spriteState.pressedSprite;
-    }
-
+public class ButtonTrigger : UIElement
+{
     //-------------------------------------------------
     protected override void OnHandHoverBegin(Hand hand)
     {
         base.OnHandHoverBegin(hand);
-        GetComponent<Button>().onClick.Invoke();
-        _buttonImage.sprite = _touchedSprite;
-    }
-
-    protected override void OnHandHoverEnd(Hand hand)
-    {
-        base.OnHandHoverEnd(hand);
-        _buttonImage.sprite = _untouchedSprite;
+        if (!hand.noSteamVRFallbackCamera)
+            InputModule.instance.Submit(gameObject);
     }
 }
