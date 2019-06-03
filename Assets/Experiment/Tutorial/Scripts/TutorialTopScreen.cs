7b5cfd9adc5ac464da6af2ddf98db49c7ed01dec
Antoine Rigitano <antoine.rigitano@cri-paris.org>
2019-06-03 17:06:14 +0200

    merged

--- Assets/Experiment/Tutorial/Scripts/TutorialTopScreen.cs
+++ Assets/Experiment/Tutorial/Scripts/TutorialTopScreen.cs
@@ -7,17 +7,22 @@ namespace CRI.HelloHouston.Experience.Tutorial
     public class TutorialTopScreen : XPElement
     {
         [SerializeField]
-        private GameObject _maintenancePanel, _secondMaintenancePanel;
+        private GameObject _maintenancePanel, _secondMaintenancePanel, _maintenancePopup, _secondMaintenancePopup;
 
         public void StartMaintenance()
         {
-
+            _maintenancePopup.SetActive(true);
         }
 
         public void ContinueMaintenance()
         {
             _maintenancePanel.SetActive(false);
             _secondMaintenancePanel.SetActive(true);
+        }
+
+        public void AdvancedMaintenance()
+        {
+            _secondMaintenancePopup.SetActive(true);
         }
     }
 }
