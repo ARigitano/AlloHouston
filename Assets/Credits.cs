ed4bb977daa0a9dc1b0b3b6833e051d03ac9792c
Antoine Rigitano <antoine@rigitano.com>
2019-08-23 16:34:14 +0200

    Credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Displays or hides the credits panel.
+/// </summary>
+public class Credits : MonoBehaviour
+{
+    /// <summary>
+    /// The credits panel.
+    /// </summary>
+    [SerializeField]
+    private GameObject _creditsPanel;
+    /// <summary>
+    /// Is the panel enabled or disabled.
+    /// </summary>
+    private bool isEnabled = false;
+
+    /// <summary>
+    /// Enables or disables the panel.
+    /// </summary>
+    public void PanelEnabler()
+    {
+        if(!isEnabled)
+        {
+            _creditsPanel.SetActive(true);
+            isEnabled = true;
+        }
+        else
+        {
+            _creditsPanel.SetActive(false);
+            isEnabled = false;
+        }
+    }
+}
